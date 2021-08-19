a801b669b2e4162e6d625b86b44a3e96a360ed39
Andy McKenna <amckenna@dealeron.com>
2021-08-19 15:49:22 -0400

    Attempt to bypass the ASPX handler

---
+++ Greenfield/Controllers/WebFormController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Greenfield.Controllers
+{
+  [ApiController]
+  [Route("WebForm.aspx")]
+  public class WebFormController : ControllerBase
+  {
+
+    private readonly ILogger<WebFormController> _logger;
+
+    public WebFormController(ILogger<WebFormController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public string Get()
+    {
+      return "GREENFIELD";
+    }
+  }
+}
---
+++ LegacyApi/CustomPageHandlerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace LegacyApi
+{
+  public class CustomPageHandlerFactory : PageHandlerFactory
+  {
+    public override IHttpHandler GetHandler(HttpContext context, string requestType, string url, string pathTranslated)
+    {
+      if (context.Request.Path.StartsWith("/WebForm.aspx", StringComparison.OrdinalIgnoreCase))
+      {
+        return null;
+      }
+
+      return base.GetHandler(context, requestType, url, pathTranslated);
+    }
+  }
+}
